5a42cfb16f6da839938973460d0aeefa3a19acc1
MrCoft <devcoft@gmail.com>
2021-10-09 04:58:33 +0200

    VSCode config

---
+++ tests/test_unittest.py
@@ -0,0 +1,10 @@
+import unittest
+
+
+class Test_TestIncrementDecrement(unittest.TestCase):
+    def test_increment(self):
+        self.assertEqual(5, 4)
+
+
+if __name__ == '__main__':
+    unittest.main()
